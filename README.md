11536061b0b0c25db07c014ae29b87924c03a63e
Chris Hayes <cwhayes711@gmail.com>
2018-11-27 10:30:46 -0500

    Convert the archived protobuf file into a human-readable text file.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # prediction-loc
+
+#### Setup
+
+`pip3 install --user protobuf`
+
+#### Usage
+
+`python getArchive.py --datetime [YYYY-MM-DDTHH:mm] --output [output file location]`
--- scripts/getArchive.py
+++ scripts/getArchive.py
@@ -2,7 +2,12 @@ import argparse
 import boto3
 import os
 import pytz
+import urllib2
 from datetime import datetime
+from sys import path
+
+path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from lib import gtfs_realtime_pb2
 
 OBJECT_PREFIX_FORMAT = "concentrate/{0}/{1}/{2}/{0}-{1}-{2}T{3}:{4}"
 DATETIME_FORMAT = "%Y-%m-%dT%H:%M"
@@ -11,20 +16,23 @@ LOCAL_TIMEZONE = pytz.timezone("US/Eastern")
 parser = argparse.ArgumentParser(description="Retrieve an archived GTFS-rt file from S3")
 parser.add_argument("-D", "--datetime", dest="datetime", required=True, help="Datetime of desired archive file, in format {YYYY}-{MM}-{DD}T{HH}:{mm}")
 parser.add_argument("-o", "--output", dest="output", required=True, help="Location for where to place the output file")
-parser.add_argument("-C", "--convert", action="store_const", const="convert", help="NOT IMPLEMENTED YET - Flag that the resulting file should be converted from protobuf to plaintext")
 args = vars(parser.parse_args())
 
 outputfile = os.path.expanduser(args["output"])
 dateTime = LOCAL_TIMEZONE.localize(datetime.strptime(args["datetime"], DATETIME_FORMAT)).astimezone(pytz.utc)
-
-bucketName = os.getenv("S3_BUCKET_NAME")
-s3 = boto3.resource("s3")
-bucket = s3.Bucket(bucketName)
-prefix = OBJECT_PREFIX_FORMAT.format(dateTime.year, dateTime.month, dateTime.day, dateTime.hour, dateTime.minute)
-objectsWithPrefix = bucket.objects.filter(Prefix=prefix)
-for obj in objectsWithPrefix:
-    if "mbta_bus_" in obj.key and "trip_updates" in obj.key:
-        print("Downloading {0}".format(obj.key))
-        bucket.download_file(obj.key, outputfile)
-        break
-print("Done.")
+with open(outputfile, "w") as file:
+    bucketName = os.getenv("S3_BUCKET_NAME")
+    s3 = boto3.resource("s3")
+    feed_message = gtfs_realtime_pb2.FeedMessage()
+    bucket = s3.Bucket(bucketName)
+    prefix = OBJECT_PREFIX_FORMAT.format(dateTime.year, dateTime.month, dateTime.day, dateTime.hour, dateTime.minute)
+    objectsWithPrefix = bucket.objects.filter(Prefix=prefix)
+    for obj in objectsWithPrefix:
+        if "mbta_bus_" in obj.key and "trip_updates" in obj.key:
+            pb_url = "https://s3.amazonaws.com/{0}/{1}".format(bucketName, obj.key)
+            print("Processing {0}...".format(pb_url))
+            response = urllib2.urlopen(pb_url)
+            feed_message.ParseFromString(response.read())
+            file.write(str(feed_message))
+            break
+    print("Done.")
